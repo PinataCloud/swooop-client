3cec67a45016f4a07699e79ece2c19589fbebf38
Steve <stevedsimkins@gmail.com>
2024-03-01 11:56:11 -0700

    Initial Commit

---
+++ SWOOOP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWOOOP
+//
+//  Created by Steve Simkins on 3/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWOOOP/SWOOOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWOOOPApp.swift
+//  SWOOOP
+//
+//  Created by Steve Simkins on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SWOOOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

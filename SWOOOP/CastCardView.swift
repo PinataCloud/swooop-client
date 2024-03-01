648bd3c9129beab9be5aa0b55a8a8019eb51dbd2
Steve <stevedsimkins@gmail.com>
2024-03-01 13:05:23 -0700

    added cards

---
+++ SWOOOP/CastCardView.swift
@@ -0,0 +1,38 @@
+//
+//  CastCard.swift
+//  SWOOOP
+//
+//  Created by Steve Simkins on 3/1/24.
+//
+
+import SwiftUI
+
+struct CastCardView: View {
+    var cast: Cast
+    
+    var body: some View {
+        VStack {
+            Text(cast.castText)
+            HStack{
+            AsyncImage(url: URL(string: cast.pfp)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 42, height: 42)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                } else if phase.error != nil {
+                    Text("Failed to load image")
+                } else {
+                    ProgressView()
+                }
+            }
+            Text("@\(cast.username)")
+                    .bold()
+            }
+        }
+        .padding()
+        .frame(maxHeight: .infinity)
+    }
+}
+
--- SWOOOP/ContentView.swift
+++ SWOOOP/ContentView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
+struct Embed: Codable {
+    let type: String
+    let url: String
+}
+
+struct Cast: Codable {
+    let id: Int
+    let castText: String
+    let embeds: [Embed]
+    let username: String
+    let pfp: String
+    let timestamp: Int
+}
+
 struct ContentView: View {
     
+    @State private var data = Cast(
+        id: 1,
+        castText: "hello world!",
+        embeds: [],
+        username: "stevedylandev.eth",
+        pfp: "https://gateway.pinata.cloud/ipfs/QmVLwvmGehsrNEvhcCnnsw5RQNseohgEkFNN1848zNzdng?filename=pinnie.png",
+        timestamp: 1
+    )
+    
     var body: some View {
-        VStack {
+        ScrollView(.vertical){
+            LazyVStack {
+                
+       CastCardView(cast: data)
+       CastCardView(cast: data)
+       CastCardView(cast: data)
+            }
         }
-        .padding()
     }
 }
 

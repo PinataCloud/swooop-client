e75e036cef73c722380ed565f76128c5efb43be3
polluterofminds <justin.edward.hunter@gmail.com>
2024-03-02 11:42:36 -0700

    sign in working

--- SWOOOP/UserManager.swift
+++ SWOOOP/UserManager.swift
@@ -16,16 +16,48 @@ struct User: Codable {
 struct SignerPayload: Codable {
     let pollingToken: String
     let privateKey: String
-    let deepLinkURL: String
-    let fid: String
+    let publicKey: String
+    let deepLinkUrl: String
 }
 
 struct Completion: Codable {
-    let status: String
+    let state: String
+    let userFid: Int
 }
 
 class UserManager {
     static let shared = UserManager()
+    
+    func getUserData() -> User? {
+        var user_name: String = ""
+        var user_fid: String = ""
+        var user_pfp: String = ""
+        if let username = KeyValueStore.shared.value(forKey: "username") as? String {
+            user_name = username
+        } else {
+            print("username not found")
+        }
+        
+        if let fid = KeyValueStore.shared.value(forKey: "fid") as? String {
+            user_fid = fid
+        } else {
+            print("fid not found")
+        }
+        
+        if let pfp = KeyValueStore.shared.value(forKey: "pfp") as? String {
+            user_pfp = pfp
+        } else {
+            print("pfp not found")
+        }
+        
+        if(user_fid != "" && user_pfp != "" && user_name != "") {
+            let newUser = User(fid: Int(user_fid)!, username: user_name, pfp: user_pfp)
+            return newUser
+        }
+        
+        return User(fid: 0, username: "", pfp: "")
+    }
+    
     func fetchUser(fid: String, completion: @escaping (Result<User, Error>) -> Void) {
         let urlString = "https://your-api-endpoint.com/user?fid=\(fid)"
         guard let url = URL(string: urlString) else {
@@ -68,7 +100,8 @@ class UserManager {
     }
     
     func pollForSuccess(token: String, completion: @escaping (Result<Completion, Error>) -> Void) {
-        let urlString = "https://your-api-endpoint.com/user?token=\(token)"
+        print("We polling...")
+        let urlString = "https://swooop-server.onrender.com/sign-in/poll?pollingToken=\(token)"
         guard let url = URL(string: urlString) else {
             completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
@@ -100,8 +133,23 @@ class UserManager {
             
             do {
                 let complete = try JSONDecoder().decode(Completion.self, from: data)
-                let status: String = complete.status
-                if (status == "complete") {
+                let status: String = complete.state
+                print(status)
+                if (status == "completed") {
+                    KeyValueStore.shared.setValue(String(complete.userFid), forKey: "fid")
+//                    self.fetchUser(fid: String(complete.userFid)) { result in
+//                        switch result {
+//                        case .success(let user):
+//                            print(user)
+//                            KeyValueStore.shared.setValue(user.username, forKey: "username")
+//                            KeyValueStore.shared.setValue(user.pfp, forKey: "pfp")
+//                        case .failure(let error):
+//                            // Handle error
+//                            print("Error: \(error)")
+//                        }
+//                    }
+                    print("WE MADE IT")
+                    KeyValueStore.shared.setValue("true", forKey: "signer_approved")
                     completion(.success(complete))
                 } else {
                     self.pollForSuccess(token: token, completion: completion)
@@ -113,7 +161,8 @@ class UserManager {
     }
     
     func signIn(completion: @escaping (Result<SignerPayload, Error>) -> Void) {
-        let url = URL(string: "https://example.com/api/endpoint")!
+        print("Signing in...")
+        let url = URL(string: "https://d5db-65-144-97-186.ngrok-free.app/sign-in")!
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -142,25 +191,17 @@ class UserManager {
                 // Decode the JSON response into a SignerPayload object
                 let decoder = JSONDecoder()
                 let signerPayload = try decoder.decode(SignerPayload.self, from: responseData)
+                print("Signer payload")
+                print(signerPayload)
                 KeyValueStore.shared.setValue(signerPayload.privateKey, forKey: "signer_private")
                 KeyValueStore.shared.setValue("false", forKey: "signer_approved")
-                KeyValueStore.shared.setValue(signerPayload.fid, forKey: "fid")
-                self.fetchUser(fid: signerPayload.fid) { result in
-                    switch result {
-                    case .success(let user):
-                        print(user)
-                        KeyValueStore.shared.setValue(user.username, forKey: "username")
-                        KeyValueStore.shared.setValue(user.pfp, forKey: "pfp")
-                    case .failure(let error):
-                        // Handle error
-                        print("Error: \(error)")
-                    }
-                }
+                KeyValueStore.shared.setValue(signerPayload.pollingToken, forKey: "polling_token")
                 completion(.success(signerPayload))
             } catch {
                 completion(.failure(error))
             }
         }
+        task.resume()
     }
 }
 
